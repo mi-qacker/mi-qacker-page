46ddbd9cde63b393948216f7da7ea0eaee443b32
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2024-08-23 11:48:54 +0300

    Set content-type in static uploader (#12)

--- tools/s3/upload-file.js
+++ tools/s3/upload-file.js
@@ -1,5 +1,6 @@
 import {PutObjectCommand} from '@aws-sdk/client-s3';
 import fs from 'fs';
+import path from 'path';
 import {s3Client} from './s3-client.js';
 import {BUCKET_NAME} from './variables.js';
 
@@ -11,11 +12,12 @@ import {BUCKET_NAME} from './variables.js';
 export async function uploadFile(filePath, key) {
 	const fileStream = fs.createReadStream(filePath);
 
-	// Параметры для загрузки файла
+	// File Upload params
 	const params = {
 		Bucket: BUCKET_NAME,
 		Key: key,
 		Body: fileStream,
+		ContentType: getContentType(filePath),
 	};
 
 	try {
@@ -24,4 +26,29 @@ export async function uploadFile(filePath, key) {
 	} catch (err) {
 		console.error(`Error uploading ${filePath}: `, err);
 	}
+}
+
+/**
+ * Get `content-type` by file ext
+ * @param {string} fileName
+ * @returns {string | undefined}
+ */
+function getContentType(fileName) {
+	const ext = path.extname(fileName).toLowerCase();
+
+	const mimeTypes = {
+		'.html': 'text/html',
+		'.css': 'text/css',
+		'.js': 'application/javascript',
+		'.json': 'application/json',
+		'.png': 'image/png',
+		'.jpg': 'image/jpeg',
+		'.gif': 'image/gif',
+		'.svg': 'image/svg+xml',
+		'.ico': 'image/x-icon',
+		'.txt': 'text/plain',
+		'.pdf': 'application/pdf',
+	};
+
+	return mimeTypes[ext] || undefined;
 }

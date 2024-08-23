289027617f89134bede343f42b02c59e222d9d41
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2024-08-23 11:24:18 +0300

    change bucket name

--- tools/s3/variables.js
+++ tools/s3/variables.js
@@ -1,4 +1,4 @@
-export const BUCKET_NAME = 'mi-qacker.ru';
+export const BUCKET_NAME = 'www.mi-qacker.ru';
 export const BUILD_FOLDER_NAME = 'dist';
 export const REGION = 'ru-central1';
 export const ENDPOINT = 'https://storage.yandexcloud.net';
